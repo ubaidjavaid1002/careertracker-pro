38ae471d202f7e41a4927ff1e9d926746f538015
ubaid <ubaidjavaid1002@gmail.com>
2026-06-05 16:34:16 +0500

    Added Django admin configuration

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "company",
+        "position",
+        "status",
+        "source",
+        "applied_date"
+    )
+
+    list_filter = (
+        "status",
+        "source"
+    )
+
+    search_fields = (
+        "company",
+        "position"
+    )
